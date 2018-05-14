33b8794725abf54f137323420d68e9612ac2644a
Tom Nicolosi <tom@teeehn.com>
2018-05-14 13:48:31 -0700

    Added a simple header.
    Just site name and description.
    Added a new mount point for the post content.

--- index.php
+++ index.php
@@ -12,7 +12,11 @@
 (function(){
   const output = document.getElementById('app');
   const state = {
-    posts: []
+    posts: [],
+    siteInfo: {
+      name: '',
+      description: ''
+    }
   }
   const getPost = e => {
     e.preventDefault();
@@ -21,10 +25,12 @@
     const title = `<h1>${post.title.rendered}</h1>`;
     const body = `<div>${post.content.rendered}</div>`;
     const article =`<article>${title}${body}</article>`;
-    output.innerHTML = article;
+    const contentContainer = document.getElementById('output');
+    contentContainer.innerHTML = article;
   }
   const getPosts = () => {
-    fetch('/wp-json/wp/v2/posts')
+    const postContent = document.createElement('div');
+    return fetch('/wp-json/wp/v2/posts')
       .then(res => res.json()) // TO DO: handle error
       .then(json => {
         state.posts = json.map(post => post);
@@ -39,11 +45,40 @@
           anchor.onclick = getPost;
           list.appendChild(item);
         });
-        output.innerHTML = '<h1>Posts</h1>';
-        output.appendChild(list);
-      });
+        postContent.innerHTML = '<h1>Posts</h1>';
+        postContent.appendChild(list);
+        return postContent;
+      })
+      .catch(err => {
+        postContent.innerHTML = err;
+        return postContent;
+      })
   }
-  getPosts();
+  const appContainer = () => {
+    let content = `<h1>${state.siteInfo.name}</h1>`;
+    content += `<h2>${state.siteInfo.description}</h2>`;
+    content += `<div id="output"></div>`;
+    return content;
+  }
+  const getSiteInfo = (state) => {
+    return fetch('/wp-json')
+      .then(res => res.json())
+      .then(json => {
+        state = Object.assign(state, {'siteInfo': {
+          description: json.description,
+          name: json.name
+        }})
+        return appContainer();
+      });     
+  }
+  async function init() {
+    const appContainerStr = await getSiteInfo(state);
+    output.innerHTML = appContainerStr;
+    const postData = await getPosts();
+    const postContainer = document.getElementById('output');
+    postContainer.appendChild(postData);
+  }
+  window.onload = init;
 }())
 </script>
 </body>
