ee493db86ae9bf9d9b880fd207f65cd1341c53f5
Tom Nicolosi <tom@teeehn.com>
2018-07-08 17:44:38 -0700

    Implemented wp_head in index.php file to allow flexibility and configuration.
    Added functions.php.
    Removed un-needed links and meta from wp_head output.

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Remove un-needed meta from the head.
+ */
+function tn_clean_head() {
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wp_shortlink_wp_head');
+    remove_action('wp_head', 'dns_prefetch');
+
+    add_filter('the_generator', '__return_false');
+    add_filter( 'emoji_svg_url', '__return_false' );
+
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );   
+    
+    // Disable REST API link tag
+    remove_action('wp_head', 'rest_output_link_wp_head', 10);
+    // Disable oEmbed Discovery Links
+    remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+    // Disable REST API link in HTTP headers
+    remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+}
+add_action('after_setup_theme', 'tn_clean_head');
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>tn headless wp theme</title>
+  <?php wp_head(); ?>
 </head>
 <body>
   <div id="root"></div>
